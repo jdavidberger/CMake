411b3fc429956fe34b8b86cdf5288edf8f2048f9
Justin Berger <j.david.berger@gmail.com>
2017-07-19 17:30:53 -0600

    debugger: json: Added protocol server

---
+++ Source/cmDebugServerJson.cxx
@@ -0,0 +1,165 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+file Copyright.txt or https://cmake.org/licensing for details.  */
+#include "cmDebugServerJson.h"
+#include "cmMakefile.h"
+
+#include "cmsys/SystemInformation.hxx"
+
+static cmsys::SystemInformation info;
+
+#if defined(CMAKE_BUILD_WITH_CMAKE)
+#include "cmConnection.h"
+#include "cm_jsoncpp_reader.h"
+#endif
+
+cmDebugServerJson::cmDebugServerJson(cmDebugger& debugger, cmConnection* conn)
+  : cmDebugServer(debugger, conn)
+{
+}
+
+cmDebugServerJson::cmDebugServerJson(cmDebugger& debugger,
+                                     const std::string& name)
+  : cmDebugServerJson(debugger,
+                      new cmPipeConnection(name, new cmServerBufferStrategy()))
+{
+}
+
+void cmDebugServerJson::ProcessRequest(cmConnection* connection,
+                                       const std::string& jsonRequest)
+{
+  Json::Reader reader;
+  Json::Value value;
+  if (!reader.parse(jsonRequest, value)) {
+    return;
+  }
+
+  auto request = value["Command"].asString();
+
+  if (request == "Break") {
+    Debugger.Break();
+  } else if (request.find("ClearBreakpoints") == 0) {
+    Debugger.ClearAllBreakpoints();
+  } else if (request.find("RemoveBreakpoint") == 0) {
+    Debugger.ClearBreakpoint(value["File"].asString(), value["Line"].asInt());
+  } else if (request.find("AddBreakpoint") == 0) {
+    Debugger.SetBreakpoint(value["File"].asString(), value["Line"].asInt());
+  } else {
+
+    auto ctx = Debugger.PauseContext();
+    if (!ctx) {
+      return;
+    }
+
+    if (request == "Continue") {
+      ctx.Continue();
+    } else if (request == "StepIn") {
+      ctx.StepIn();
+    } else if (request == "StepOut") {
+      ctx.StepOut();
+    } else if (request == "StepOver") {
+      ctx.Step();
+    } else if (request.find("Evaluate") == 0) {
+      auto requestVal = value["Request"].asString();
+      const char* v = CM_NULLPTR;
+      if (!requestVal.empty() && requestVal[0] == '"' &&
+          requestVal.back() == '"') {
+        v = ctx.GetMakefile()->ExpandVariablesInString(requestVal);
+      } else {
+        v = ctx.GetMakefile()->GetDefinition(requestVal);
+      }
+
+      value.removeMember("Command");
+      if (v) {
+        value["Response"] = std::string(v);
+      } else {
+        value["Response"] = false;
+      }
+      connection->WriteData(value.toStyledString());
+    }
+  }
+}
+
+void cmDebugServerJson::SendStateUpdate(cmConnection* connection)
+{
+  std::string state = "";
+  Json::Value value;
+  value["PID"] = info.GetProcessId();
+
+  switch (Debugger.CurrentState()) {
+    case cmDebugger::State::Running:
+      value["State"] = "Running";
+      break;
+    case cmDebugger::State::Paused: {
+      value["State"] = "Paused";
+      Json::Value back(Json::arrayValue);
+
+      if (auto ctx = Debugger.PauseContext()) {
+        auto currentLine = ctx.CurrentLine();
+        auto backtrace = ctx.GetBacktrace();
+        int id = 0;
+        while (!backtrace.Top().FilePath.empty()) {
+          auto line = Json::Value::Int(backtrace.Top().Line);
+          if (line != 0) {
+            Json::Value frame(Json::objectValue);
+            frame["ID"] = id++;
+            frame["File"] = backtrace.Top().FilePath;
+            frame["Line"] = line;
+            frame["Name"] = backtrace.Top().Name;
+
+            switch (backtrace.GetBottom().GetType()) {
+              case cmStateEnums::BaseType:
+                frame["Type"] = "BaseType";
+                break;
+              case cmStateEnums::BuildsystemDirectoryType:
+                frame["Type"] = "BuildsystemDirectoryType";
+                break;
+              case cmStateEnums::FunctionCallType:
+                frame["Type"] = "FunctionCallType";
+                break;
+              case cmStateEnums::MacroCallType:
+                frame["Type"] = "MacroCallType";
+                break;
+              case cmStateEnums::IncludeFileType:
+                frame["Type"] = "IncludeFileType";
+                break;
+              case cmStateEnums::InlineListFileType:
+                frame["Type"] = "InlineListFileType";
+                break;
+              case cmStateEnums::PolicyScopeType:
+                frame["Type"] = "PolicyScopeType";
+                break;
+              case cmStateEnums::VariableScopeType:
+                frame["Type"] = "VariableScopeType";
+                break;
+            }
+
+            back.append(frame);
+          }
+          backtrace = backtrace.Pop();
+        }
+        value["Backtrace"] = back;
+      }
+
+    } break;
+    case cmDebugger::State::Unknown:
+      return;
+  }
+
+  if (connection && connection->IsOpen()) {
+    connection->WriteData(value.toStyledString());
+  }
+}
+
+void cmDebugServerJson::OnChangeState()
+{
+  cmDebuggerListener::OnChangeState();
+  std::lock_guard<std::recursive_mutex> l(ConnectionsMutex);
+  for (auto& connection : Connections) {
+    SendStateUpdate(connection.get());
+  }
+}
+
+void cmDebugServerJson::OnConnected(cmConnection* connection)
+{
+  SendStateUpdate(connection);
+}
---
+++ Source/cmDebugServerJson.h
@@ -0,0 +1,46 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+file Copyright.txt or https://cmake.org/licensing for details.  */
+#pragma once
+
+#include "cmDebugServer.h"
+#include "cmDebugServerConsole.h"
+#include "cmDebugger.h"
+#include "cmServer.h"
+#include "cmServerConnection.h"
+
+/***
+ * Debug server which communicates via JSON objects.
+ */
+class cmDebugServerJson : public cmDebugServer
+{
+public:
+  /***
+   * Starts a json server using a socket with a json connection strategy
+   *
+   * @param debugger The debugger to attach to
+   * @param pipe The named pipe to use
+   */
+  cmDebugServerJson(cmDebugger& debugger, const std::string& pipe);
+
+  /***
+   * General purpose constructor. It is expected that the buffering strategy
+   * only ever gives valid JSON objects.
+   * @param debugger The debugger to attach to
+   * @param conn Any valid connection
+   */
+  cmDebugServerJson(cmDebugger& debugger, cmConnection* conn);
+
+  ~cmDebugServerJson() override { Close(); }
+
+  void ProcessRequest(cmConnection* connection,
+                      const std::string& request) override;
+  void SendStateUpdate(cmConnection* connection);
+  void OnChangeState() override;
+
+  void OnConnected(cmConnection* connection) override;
+};
+
+/***
+ * Creates a buffering strategy which chunks data as JSON objects
+ */
+std::unique_ptr<cmConnectionBufferStrategy> CreateJsonConnectionStrategy();
