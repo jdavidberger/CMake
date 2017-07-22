3faa7e73037808df414b46033f767acd114b84c1
Justin Berger <j.david.berger@gmail.com>
2017-07-22 09:36:58 -0600

    debugger: Refactor to allow broadcasts from Q

--- Source/cmDebugServer.cxx
+++ Source/cmDebugServer.cxx
@@ -4,10 +4,18 @@ file Copyright.txt or https://cmake.org/licensing for details.  */
 #include "cmDebugServerConsole.h"
 #include "cmMakefile.h"
 
+static void __broadcast(uv_async_t* arg)
+{
+  auto server = reinterpret_cast<cmDebugServer*>(arg->data);
+  server->ProcessBroadcastQueue();
+}
+
 cmDebugServer::cmDebugServer(cmDebugger& debugger, cmConnection* conn)
   : cmServerBase(conn)
   , cmDebuggerListener(debugger)
 {
+  this->BroadcastQueueSignal.init(Loop, __broadcast);
+  this->BroadcastQueueSignal->data = this;
 }
 
 bool cmDebugServer::OnSignal(int signum)
@@ -17,4 +25,45 @@ bool cmDebugServer::OnSignal(int signum)
     return true;
   }
   return false;
+}
+
+void cmDebugServer::StartShutDown()
+{
+  std::unique_lock<std::mutex> l(BroadcastQueueMutex);
+  BroadcastQueueSignal.reset();
+  cmServerBase::StartShutDown();
+}
+
+void cmDebugServer::AsyncBroadcast(const std::string& msg)
+{
+  if(msg.size() == 0)
+      return;
+  std::lock_guard<std::mutex> l(BroadcastQueueMutex);
+  BroadcastQueue.push_back(msg);
+  this->BroadcastQueueSignal.send();
+}
+
+void cmDebugServer::Broadcast(const std::string& msg)
+{
+  std::lock_guard<std::recursive_mutex> l(ConnectionsMutex);
+  for (auto& connection : Connections) {
+    if(connection->IsOpen()) {
+      connection->WriteData(msg);
+    }
+  }
+}
+
+void cmDebugServer::ProcessBroadcastQueue()
+{
+  std::lock_guard<std::mutex> l(BroadcastQueueMutex);
+  for (auto& msg : BroadcastQueue) {
+    this->Broadcast(msg);
+  }
+  this->BroadcastQueue.clear();
+  BroadcastCV.notify_all();
+}
+
+cmDebugServer::~cmDebugServer()
+{
+  Close();
 }
--- Source/cmDebugServer.h
+++ Source/cmDebugServer.h
@@ -4,10 +4,29 @@ file Copyright.txt or https://cmake.org/licensing for details.  */
 
 #include "cmDebugger.h"
 #include "cmServer.h"
+#include <condition_variable>
+#include <iostream>
+
+#include "cmAutoHandle.h"
 
 class cmDebugServer : public cmServerBase, public cmDebuggerListener
 {
 public:
   cmDebugServer(cmDebugger& debugger, cmConnection* conn);
+
+  virtual ~cmDebugServer();
+
   virtual bool OnSignal(int signum) override;
+  void StartShutDown() override;
+
+  virtual void Broadcast(const std::string& msg);
+  void AsyncBroadcast(const std::string& msg);
+  void ProcessBroadcastQueue();
+
+private:
+  auto_async_t BroadcastQueueSignal;
+
+  std::condition_variable BroadcastCV;
+  std::vector<std::string> BroadcastQueue;
+  std::mutex BroadcastQueueMutex;
 };
