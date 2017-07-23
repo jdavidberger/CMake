304afadc158a0afa0cde762d42765a01f1838ecd
Justin Berger <j.david.berger@gmail.com>
2017-07-23 11:54:42 -0600

    server: Fixed minor memory leaks

--- Source/cmServer.cxx
+++ Source/cmServer.cxx
@@ -56,10 +56,6 @@ cmServer::cmServer(cmConnection* conn, bool supportExperimental)
 
 cmServer::~cmServer()
 {
-  if (!this->Protocol) { // Server was never fully started!
-    return;
-  }
-
   for (cmServerProtocol* p : this->SupportedProtocols) {
     delete p;
   }
@@ -109,6 +105,7 @@ void cmServer::ProcessRequest(cmConnection* connection,
 void cmServer::RegisterProtocol(cmServerProtocol* protocol)
 {
   if (protocol->IsExperimental() && !this->SupportExperimental) {
+    delete protocol;
     return;
   }
   auto version = protocol->ProtocolVersion();
