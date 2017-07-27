76fdcd4347ebae36fe710a0c524cbf09261dede4
Justin Berger <j.david.berger@gmail.com>
2017-07-27 07:11:07 -0600

    server: Added auto handle

---
+++ Source/cmAutoHandle.cxx
@@ -0,0 +1,128 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+   file Copyright.txt or https://cmake.org/licensing for details.  */
+#include <assert.h>
+
+#include "cmAutoHandle.h"
+#include "cm_uv.h"
+
+template <typename T>
+auto_handle_base_<T>::~auto_handle_base_()
+{
+  reset();
+}
+
+template <typename T>
+void auto_handle_base_<T>::allocate(void* data)
+{
+  reset();
+  handle = new T();
+  handle->data = data;
+}
+
+template <typename T>
+static void close_delete(uv_handle_t* h)
+{
+  delete reinterpret_cast<T*>(h);
+}
+
+template <typename T>
+void auto_handle_base_<T>::reset()
+{
+  if (handle) {
+    assert(!uv_is_closing(*this));
+    if (!uv_is_closing(*this)) {
+      uv_close(*this, &close_delete<T>);
+    }
+    handle = CM_NULLPTR;
+  }
+}
+
+template <typename T>
+auto_handle_base_<T>::operator uv_handle_t*()
+{
+  return reinterpret_cast<uv_handle_t*>(handle);
+}
+
+template <typename T>
+auto_handle_<T>::operator T*()
+{
+  return this->handle;
+}
+
+template <typename T>
+auto_handle_base_<T>::auto_handle_base_()
+{
+}
+
+template <typename T>
+void auto_handle_base_<T>::allocate()
+{
+  allocate(CM_NULLPTR);
+}
+
+void auto_async_t::send()
+{
+  std::lock_guard<std::mutex> lock(handleMutex);
+  if (this->handle) {
+    uv_async_send(*this);
+  }
+}
+
+void auto_async_t::reset()
+{
+  std::lock_guard<std::mutex> lock(handleMutex);
+  auto_handle_base_<uv_async_t>::reset();
+}
+
+int auto_async_t::init(uv_loop_t& loop, uv_async_cb async_cb, void* data)
+{
+  allocate(data);
+  return uv_async_init(&loop, handle, async_cb);
+}
+
+auto_async_t::~auto_async_t()
+{
+  reset();
+}
+
+int auto_signal_t::init(uv_loop_t& loop, void* data)
+{
+  allocate(data);
+  return uv_signal_init(&loop, handle);
+}
+
+int auto_signal_t::start(uv_signal_cb cb, int signum)
+{
+  assert(handle);
+  return uv_signal_start(*this, cb, signum);
+}
+
+void auto_signal_t::reset()
+{
+  stop();
+  auto_handle_::reset();
+}
+
+void auto_signal_t::stop()
+{
+  if (handle) {
+    uv_signal_stop(*this);
+  }
+}
+
+int auto_pipe_t::init(uv_loop_t& loop, int ipc, void* data)
+{
+  allocate(data);
+  return uv_pipe_init(&loop, *this, ipc);
+}
+
+auto_pipe_t::operator uv_stream_t*()
+{
+  return reinterpret_cast<uv_stream_t*>(handle);
+}
+
+#define GENERATE_EXPLICIT_INITIALIZATIONS(type, NAME)                         \
+  template class auto_handle_base_<uv_##NAME##_t>;                            \
+  template class auto_handle_<uv_##NAME##_t>;
+UV_HANDLE_TYPE_MAP(GENERATE_EXPLICIT_INITIALIZATIONS);
+#undef GENERATE_EXPLICIT_INITIALIZATIONS
---
+++ Source/cmAutoHandle.h
@@ -0,0 +1,103 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+   file Copyright.txt or https://cmake.org/licensing for details.  */
+#pragma once
+#include "cm_uv.h"
+
+#include <cmConfigure.h>
+#include <mutex>
+
+/***
+ * RAII class to simplify and insure the safe usage of uv_*_t types.
+ *
+ * @tparam T actual uv_*_t type represented.
+ */
+template <typename T>
+class auto_handle_base_
+{
+  CM_DISABLE_COPY(auto_handle_base_);
+
+protected:
+  T* handle = CM_NULLPTR;
+
+  /**
+   * Allocate memory for the type and optionally set it's 'data' pointer.
+   * Protected since
+   * this should only be called for an appropriate 'init' call.
+   *
+   * @param data data pointer to set
+   */
+  virtual void allocate(void* data);
+  void allocate();
+
+public:
+  auto_handle_base_();
+  virtual ~auto_handle_base_();
+  /**
+   * Properly close the handle if needed and sets the inner handle to nullptr
+   *
+   * For completely safe operation, if you override reset you must provide a
+   * dtor which calls reset.
+   */
+  virtual void reset();
+
+  /**
+   * Allow less verbose calling of uv_handle_* functions
+   * @return reinterpreted handle
+   */
+  operator uv_handle_t*();
+};
+
+template <typename T>
+class auto_handle_ : public auto_handle_base_<T>
+{
+public:
+  /***
+   * Allow less verbose calling of uv_<T> functions
+   * @return reinterpreted handle
+   */
+  operator T*();
+};
+
+/***
+ * This specialization is required to avoid duplicate 'operator uv_handle_t*()'
+ * declarations
+ */
+template <>
+class auto_handle_<uv_handle_t> : public auto_handle_base_<uv_handle_t>
+{
+};
+
+class auto_async_t : public auto_handle_<uv_async_t>
+{
+  /***
+   * Wile uv_async_send is itself thread-safe, there are
+   * no strong guarantees that close hasn't already been
+   * called on the handle; and that it might be deleted
+   * as the send call goes through. This mutex guards
+   * against that.
+   */
+  std::mutex handleMutex;
+
+public:
+  int init(uv_loop_t& loop, uv_async_cb async_cb, void* data = CM_NULLPTR);
+  ~auto_async_t() override;
+
+  void send();
+
+  void reset() override;
+};
+
+struct auto_signal_t : public auto_handle_<uv_signal_t>
+{
+  virtual int init(uv_loop_t& loop, void* data = CM_NULLPTR);
+  int start(uv_signal_cb cb, int signum);
+
+  void reset() override;
+  void stop();
+};
+
+struct auto_pipe_t : public auto_handle_<uv_pipe_t>
+{
+  operator uv_stream_t*();
+  int init(uv_loop_t& loop, int ipc, void* data = CM_NULLPTR);
+};
